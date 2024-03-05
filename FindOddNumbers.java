5021b3a73447013579a021afd68c6307f233f4ba
kalaiselvikarthikeyan <132609118+kalaiselvikarthikeyan@users.noreply.github.com>
2024-03-05 13:47:42 +0100

    Add files via upload
    Odd Number from 1 to 10

---
+++ FindOddNumbers.java
@@ -0,0 +1,18 @@
+package homeAssignments;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		int maxRange=10;
+		System.out.println("Odd Numbers 1 to 10 :");
+		for (int i = 1; i <=maxRange; i++) {
+			if(i%2!=0) {
+				System.out.println(i);
+			}
+			
+			
+		}
+
+	}
+
+}
